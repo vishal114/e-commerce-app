13fa831705acd106ffdc8a17c662a0614d425721
Vishal Agarwal <vishi111144@gmail.com>
2020-02-26 12:08:27 +0530

    updated shop data with new data, updated card utils to group items in card reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+
 import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        collections: SHOP_DATA
-      };
-    }
+  constructor(props) {
+    super(props);
 
-    render(){
-        const { collections } = this.state;
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
     return (
       <div className='shop-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
@@ -20,7 +22,7 @@ class ShopPage extends React.Component {
         ))}
       </div>
     );
-    }
+  }
 }
 
 export default ShopPage;
